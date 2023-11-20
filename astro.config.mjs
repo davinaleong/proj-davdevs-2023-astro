4fb0d2fde6058a5b555f8d6607bf2831ba2b9c11
Davina Leong <davina.leong.sy@gmail.com>
2023-11-20 20:59:37 +0800

    Revamp CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), tailwind()]
 });
